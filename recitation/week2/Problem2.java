f9c00672ac5dba470191da5ff09c230111dac5f5
Seunghan <Seunghan@Seunghan>
2017-01-28 21:55:36 -0600

    nyang

--- recitation/week2/Problem2.java
+++ recitation/week2/Problem2.java
@@ -15,7 +15,9 @@ public class Problem2 {
 	 */
 	
 	/*
-	 * (d)
+	 * (d) log(2^f(n)) = f(n) <= cg(n) = clog(2^g(n)) for n >= n_0
+	 *     => log(2^f(n)) = O(log(2^g(n)))
+	 *     => 2^f(n) = O(2^g(n))
 	 */
 	
 	/*
@@ -23,7 +25,9 @@ public class Problem2 {
 	 */
 	
 	/*
-	 * (f)
+	 * (f) f(n) <= cg(n) for n >= n_0
+	 *     => g(n) >= (1/c)f(n) = df(n) for n >= n_0
+	 *     => g(n) = OMEGA(f(n))
 	 */
 	
 	/*
@@ -31,6 +35,8 @@ public class Problem2 {
 	 */
 	
 	/*
-	 * (h)
+	 * (h) f(n) + o(f(n)) >= f(n) => OMEGA(f(n))
+	 *     f(n) + o(f(n)) <= f(n) + cf(n) = (c+1)f(n) = df(n) => O(f(n))
+	 *     => f(n) + o(f(n)) = THETA(f(n))
 	 */
 }
