2ec00ae1d6c07783ad0e26eeae3d86509ab44c98
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2017-02-19 17:01:57 -0600

    recit 5

--- recitation/week5/Problem8.java
+++ recitation/week5/Problem8.java
@@ -11,10 +11,15 @@ public class Problem8 {
 	 * T(n) = T(n/2) + 1 <= clg(n/2) + 1 = c(lgn-lg2) + 1 = clgn - c + 1 <= clgn = O(lgn)
 	 * 
 	 * Problem 4.3-3 (p87)
-	 * T(n) = 2T(n/2) + n
+	 * T(n) = 2T(n/2) + n >= 2c(n/2)lg(n/2) + n = cnlg(n/2) + n
+	 *      >= cnlg(n/2) = cn(lgn-lg2) = cnlgn - cn >= (1/2)cnlgn = OMEGA(nlgn)
+	 * => Since T(n) = O(nlgn) and T(n) = OMEGA(nlgn), T(n) = THETA(nlgn)
 	 * 
 	 * Problem 4.3-5 (p87)
-	 * 
+	 * Merge Sort: T(n) = 2T(n/2) + cn
+	 * => T(n) <= 2c(n/2)lg(n/2) + cn = cn(lgn-lg2) + cn = cnlgn - cn + cn = cnlgn = O(nlgn)
+	 * => T(n) >= 2c(n/2)lg(n/2) + cn = cn(lgn-lg2) + cn = cnlgn - cn + cn = cnlgn = OMEGA(nlgn)
+	 * => T(n) = THETA(n)
 	 */
 	
 }
--- recitation/week5/Problem9.java
+++ recitation/week5/Problem9.java
@@ -6,5 +6,5 @@ public class Problem9 {
 	 * Given an infinite number of quarters, dimes, nickels,and pennies,
 	 * write code to calculate the number of ways of representing n cents.
 	 */
-	
+
 }
