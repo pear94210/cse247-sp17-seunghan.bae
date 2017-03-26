9eff74bc13a95313bbfd32e434197721678bebfc
Seunghan <Seunghan@Seunghan.dhcp.wustl.edu>
2017-03-26 15:50:50 -0500

    recit9

---
+++ recitation/week9/Problem10.java
@@ -0,0 +1,23 @@
+package week9;
+
+public class Problem10 {
+
+	/*
+	 * Problem 11.2-2
+	 * 
+	 * Demonstrate what happens when we insert the keys 5; 28; 19; 15; 20; 33; 12; 17; 10
+	 * into a hash table with collisions resolved by chaining. Let the table have 9 slots,
+	 * and let the hash function be h(k) = kmod9
+	 * 
+	 * 0: 
+	 * 1: 28 <-> 19 <-> 10
+	 * 2: 20
+	 * 3: 12
+	 * 4: 
+	 * 5: 5
+	 * 6: 15 <-> 33
+	 * 7: 
+	 * 8: 17
+	 */
+	
+}
---
+++ recitation/week9/Problem11.java
@@ -0,0 +1,42 @@
+package week9;
+
+public class Problem11 {
+
+	/*
+	 * Problem 11.2-3
+	 * 
+	 * Professor Marley hypothesizes that he can obtain substantial performance gains
+	 * by modifying the chaining scheme to keep each list in sorted order.
+	 * How does the professor¡¯s modification affect the running time for successful searches,
+	 * unsuccessful searches, insertions, and deletions? 
+	 * 
+	 * Use "binary search" - THETA(logn)
+	 * - successful searches: THETA(logn)
+	 * - unsuccessful searches: THETA(logn)
+	 * - insertions: THETA(n)
+	 * - deletions: THETA(n)
+	 * 
+	 * What are the two rules of int hashCode()?
+	 * 
+	 * 1) no two objects that are different may have the same hashcode
+	 * 2) two objects that are identical should have the same hashcode
+	 * 
+	 * What is the worst-case time complexity of fetching an object from a hash table
+	 * that resolves collisions with chaining? Why?
+	 * 
+	 * THETA(n)
+	 * => all the objects in one bucket, and the object we want in the middle of the list
+	 * 
+	 * How do you measure the effectiveness of a hashCode() function?
+	 * 
+	 * - Do different objects have different hashcodes?
+	 * - Do same objects have same hashcodes?
+	 * - Are objects dispersed evenly among buckets?
+	 * 
+	 * How does the hashcode for set differ from the hashcode for list?
+	 * 
+	 * set: order does not matter; value matters => same value, same hashcode
+	 * list: order matters => even for elements with same value, different hashcode
+	 */
+	
+}
