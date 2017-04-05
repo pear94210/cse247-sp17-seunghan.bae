e0d741bb8eca71bc816c8893560a1c0d38314b55
seunghan <seunghan@seunghans-mbp.dhcp.wustl.edu>
2017-04-05 16:27:42 -0500

    lab 9

--- labs/rabinkarp/RK.java
+++ labs/rabinkarp/RK.java
@@ -9,6 +9,7 @@ public class RK {
 	//
 	char[] array;
 	int hash;
+	int power;
 
 	/**
 	 * Rabin-Karp string matching for a window of the specified size
@@ -17,6 +18,8 @@ public class RK {
 	public RK(int m) {
 		array = new char[m];
 		hash = 0;
+		power = 1;
+		for (int i = 0; i < m; i++) power = (power * 31) % 511;
 	}
 	
 
@@ -30,11 +33,11 @@ public class RK {
 		for (int i = 0; i < array.length - 1; i++) array[i] = array[i + 1];
 		array[array.length - 1] = d;
 		
-		int hash1 = (hash * 31);
-		int hash2 = (int)Math.pow(31, array.length) % 511;
+		int hash1 = hash * 31;
+		int hash2 = (power * temp) % 511;
 		
-		hash = ((hash1 + (int)d) - (((hash2 * temp) % 511) % 511)) % 511;
-		if (hash < 0) hash = hash + 511;
+		hash = ((hash1 + (int)d) - (hash2 % 511)) % 511;
+		if (hash < 0) hash += 511;
 				
 		return hash;
 	}
